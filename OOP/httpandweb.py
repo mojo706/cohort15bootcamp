07e89ba7234c1e057878b4b5eee7ff78720cfc36
chuchu <xriseugene@gmail.com>
2017-02-08 21:38:29 +0300

    Added comments to show my logic

--- OOP/httpandweb.py
+++
@@ -1,3 +0,0 @@
-"""
-Console App for interacting with the twitter API from python for Andela Bootcamp15
-"""
--- OOP/httpandweb/httpandweb.py
+++ OOP/httpandweb/httpandweb.py
@@ -5,17 +5,17 @@ Console App for interacting with the locu API from python for Andela Bootcamp15
 import urllib2
 import json
 
-locu_api = '9fb8cd70cb34cab8e83690473133f51943b5c93f'
+locu_api = '9fb8cd70cb34cab8e83690473133f51943b5c93f' #storing our api key in a variable for easier access
 
-def main(query):
+def main(query): #query in this case is a search
     api_key = locu_api
     url = 'https://api.locu.com/v1_0/venue/search/?api_key=' + api_key
-    locality = query.replace(' ', '%20')
-    final_url = url + "&locality=" + locality + "&category=restaurant"
+    locality = query.replace(' ', '%20') # replaced space with %20 which reps space in url :)
+    final_url = url + "&locality=" + locality + "&category=restaurant" # Magic
     json_obj = urllib2.urlopen(final_url)
     data = json.load(json_obj)
 
-    for restaurant in data['objects']:
+    for restaurant in data['objects']: 
         print restaurant['name'], restaurant['phone']
 
 if __name__ == "__main__":
